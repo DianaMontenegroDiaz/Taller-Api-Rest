e1f94d71534cc429662dfa31b5e56171208a3dab
Skamy83 <skamy1983@gmail.com>
2024-11-28 08:17:31 -0500

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -9,3 +9,21 @@ describe('get/clientes',() => {
 });
     
 });
+
+describe('get/productos',() => {
+
+    it ('retornar un codigo 200', async () => {
+        const response = await request(app).get('/productos');
+        expect(response.text).toBe('Ejemplo 2 de prueba unitaria');
+});
+    
+});
+
+describe('get/clientes/:id_cliente',() => {
+
+    it ('retornar un codigo 200', async () => {
+        const response = await request(app).get('/clientes/:id_cliente');
+        expect(response.text).toBe('Ejemplo 3 de prueba unitaria');
+});
+    
+});
