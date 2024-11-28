91ea164591c1ea6f8d0b9ba8711f03f45b7a48a6
Skamy83 <skamy1983@gmail.com>
2024-11-27 21:44:54 -0500

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = required ('../src/app');
 
-describe('get/',() => {
+describe('get/....',() => {
 
     it ('retornar un codigo 200', async () => {
         const response = await request(app).get('/...');
