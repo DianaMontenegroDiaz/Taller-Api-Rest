891bfe5561cd29874400fb2326ecd89f363000b2
Skamy83 <skamy1983@gmail.com>
2024-11-27 21:44:04 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,2 +1,12 @@
 const express = require("express");
 const app = express();
+
+app.get('/...', (req, res) => {
+    res.send('Ejemplo de prueba unitaria');
+});
+
+app.get('/...', (req, res) => {
+    res.send('Ejemplo 2 de prueba unitaria');
+});
+
+module.exports = app;
