a533439659670b92f9e481c3b974fb71b71d3d4c
Skamy83 <skamy1983@gmail.com>
2024-11-29 22:21:19 -0500

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,14 +1,17 @@
 const request =require('supertest');
 const app =require('../src/app');
 
-describe('get/clientes',() => {
+describe('get/clientes/5',() => {
 
     it ('retornar un codigo 200', async () => {
-        const response = await request(app).get('/clientes');
-        expect(response.text).toBe('Ejemplo 1 de prueba unitaria');
-        expect(response.body).toBe([
-            { id_cliente: 5, nombre_cliente: "Pedro Paramo", direccion_cliente: "Calle 09 #10-17", celular_cliente: "3208865439" }
-        ]);
+        const response = await request(app).get('/clientes/5');
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({
+            id_cliente: 5, 
+            nombre_cliente: "Pedro Paramo", 
+            direccion_cliente: "Calle 09 #10-17", 
+            celular_cliente: "3208865439" 
+        });
 });
     
 });
